245b9b8f81f4a56df02b3cd32a9997cfc627ec91
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2024-03-05 16:18:30 +0100

    added code to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,10 @@ import './App.css';
 
 function App() {
     const [data, setData] = useState(response.results);
-    const [request, setRequest] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
 
     const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
         const searchTerm = e.target.value;
-        setRequest(searchTerm);
         const filteredData = response.results.filter((d) => d.name.startsWith(searchTerm));
         setData(filteredData);
         if (filteredData.length === 0 && searchTerm.trim() !== "") {
